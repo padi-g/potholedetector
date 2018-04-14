d7dcdb51bc7e22a3f0f213c761abea22860285d8
kanishk98 <kanishk.kakar@gmail.com>
2018-04-14 17:17:02 +0530

    Utility class created

---
+++ app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/S3UploadSevice.java
@@ -0,0 +1,16 @@
+package org.reapbenefit.gautam.intern.potholedetectorbeta;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class S3UploadSevice extends Service {
+    public S3UploadSevice() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
---
+++ app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Util.java
@@ -0,0 +1,8 @@
+package org.reapbenefit.gautam.intern.potholedetectorbeta;
+
+/**
+ * Created by kanis on 14-04-2018.
+ */
+
+public class Util {
+}
