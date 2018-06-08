69e00fcf8568b802fcff0474cfaf120d86a5694a
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-06-08 11:34:10 +0530

    Implemented map in OverviewFragment

--- app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Fragments/OverviewFragment.java
+++ app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Fragments/OverviewFragment.java
@@ -1,9 +1,12 @@
 package org.reapbenefit.gautam.intern.potholedetectorbeta.Fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
@@ -14,9 +17,18 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 
 import org.reapbenefit.gautam.intern.potholedetectorbeta.R;
 
@@ -28,12 +40,22 @@ public class OverviewFragment extends Fragment implements
         GoogleApiClient.OnConnectionFailedListener,
         LocationSource.OnLocationChangedListener {
 
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
     private OnFragmentInteractionListener mListener;
-
+    private MapView mapView;
+    private LocationCallback locationCallback;
+    private LocationSettingsRequest locationSettingsRequest;
+    private LocationRequest locationRequest;
+    private Location currentLocation;
+    private LatLng currentLatLng;
+    private GoogleMap googleMap;
+    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
     }
 
     @Override
@@ -41,7 +63,93 @@ public class OverviewFragment extends Fragment implements
                              Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_overview, container, false);
 
+        createLocationCallback();
+        createLocationRequest();
+        buildLocationSettingsRequest();
+        mapView = fragmentView.findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        if (mapView != null)
+            mapView.getMapAsync((OnMapReadyCallback) this);
+        getMarkers();
         return fragmentView;
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private void startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+    }
+
+    private void getMarkers() {
+    }
+
+    private void drawMarkers() {
+    }
+
+    private void createLocationCallback() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                currentLocation = locationResult.getLastLocation();
+                currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18.0f));
+            }
+        };
+    }
+
+    @SuppressLint("RestrictedApi")
+    private void createLocationRequest() {
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+        locationRequest.setFastestInterval(UPDATE_INTERVAL_IN_MILLISECONDS / 5);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap map) {
+        this.googleMap = map;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.setMyLocationEnabled(true);
+        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        drawMarkers();
+        googleApiClient = new GoogleApiClient.Builder(getActivity()).addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onResume() {
+    super.onResume();
+        if (mapView != null) {
+            if (googleMap != null && !(ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+                googleMap.setMyLocationEnabled(true);
+                googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+                getMarkers();
+            }
+            mapView.onResume();
+        }
+        startLocationUpdates();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 
     @Override
@@ -62,27 +170,33 @@ public class OverviewFragment extends Fragment implements
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
+    public void onDestroyView() {
+        super.onDestroyView();
+        if (mapView != null)
+            mapView.onDestroy();
+    }
 
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        if (mapView != null)
+            mapView.onLowMemory();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
     }
 
     @Override
     public void onConnectionSuspended(int i) {
-
     }
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
     }
 
     @Override
     public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
 
     }
 
