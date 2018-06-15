8ffc0dadfe6082ff3babccf249b120c56d47b09d
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-06-15 12:33:32 +0530

    Implemented notification timer

--- app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Activities/TransitionsReceiver.java
+++ app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Activities/TransitionsReceiver.java
@@ -16,8 +16,13 @@ import android.util.Log;
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
+import org.reapbenefit.gautam.intern.potholedetectorbeta.BuildConfig;
 import org.reapbenefit.gautam.intern.potholedetectorbeta.Core.ApplicationClass;
 import org.reapbenefit.gautam.intern.potholedetectorbeta.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /*
 TransitionReciever contains the logic for detection of the current activity. 
@@ -32,6 +37,7 @@ public class TransitionsReceiver extends IntentService {
     private PendingIntent pendingIntent;
     private NotificationCompat.Builder builder;
     private NotificationManagerCompat notificationManagerCompat;
+    private int timer;
     public TransitionsReceiver() {
         super("TransitionsReceiver");
     }
@@ -39,6 +45,7 @@ public class TransitionsReceiver extends IntentService {
     @SuppressLint("RestrictedApi")
     @Override
     public void onHandleIntent(Intent intent) {
+        timer = sharedPreferences.getInt("timer", 0);
         //setting up notification system
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(ApplicationClass.getInstance());
         editor = sharedPreferences.edit();
@@ -50,7 +57,7 @@ public class TransitionsReceiver extends IntentService {
                 mainIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder = new NotificationCompat.Builder(this,
                 getString(R.string.notification_channel_id))
-                .setSmallIcon(R.drawable.ic_directions_car_black_24dp)
+                .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("Are you in a car?")
                 .setContentText("Tap to start logging potholes.")
                 .setPriority(NotificationManager.IMPORTANCE_HIGH)
@@ -66,10 +73,15 @@ public class TransitionsReceiver extends IntentService {
             editor.putString("currentActivity", detectedActivity.toString());
             editor.commit();
             if (detectedActivity.toString().contains("VEHICLE") && !ApplicationClass.getInstance().isTripInProgress()
-                    && detectedActivity.getConfidence() >= 40) {
+                    && detectedActivity.getConfidence() >= 40 && timer >= 60) {
                 //sending notification to user
                 notificationManagerCompat.notify(0, builder.build());
+                long currentTime = Calendar.getInstance().getTime().getTime();
+                timer = (int) TimeUnit.MILLISECONDS.toMinutes(currentTime - timer);
+                editor.putInt("timer", timer);
+                editor.commit();
             }
+            else if (timer < 60) ;
             else {
                 if (notificationManagerCompat != null) {
                     notificationManagerCompat.cancel(0);
