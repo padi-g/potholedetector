4e79ebe40e95599ed5cfbe92d7ce8720375f65dd
Kanishk Kakar <kanishk.kakar@gmail.com>
2018-06-16 12:17:03 +0530

    Replaced probable pothole marker with grey icon

--- app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Core/LoggerService.java
+++ app/src/main/java/org/reapbenefit/gautam/intern/potholedetectorbeta/Core/LoggerService.java
@@ -256,6 +256,12 @@ public class LoggerService extends Service implements SensorEventListener {
                 }
                 mCurrentLocation = location;
                 speed = location.getSpeed();
+                float speedAccuracy = 0;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    speedAccuracy = location.getSpeedAccuracyMetersPerSecond();
+                    Log.d("speedAccuracy", speedAccuracy + "");
+                }
+
                 mLastUpdateTime = getCurrentTime();
                 LocData = getLocData();
             }
